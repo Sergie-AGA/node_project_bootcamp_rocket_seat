113ae7917fa9b7eeed87c8ef836197bd42c467b2
Sergie <sergiearruda@gmail.com>
2021-02-28 19:04:05 +0000

    Creates first testing route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import routes from './routes'
+import routes from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
-// This file is missing some changes
+app.use(express.json())
+app.use(routes)
 
-app.get("/", (request, response) => {
-return response.json({ message: "Hello world" });
-});
 
 app.listen(3333, () => {  console.log("Server started on port 3333");
 });
